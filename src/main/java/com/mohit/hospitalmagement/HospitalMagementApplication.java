49f3f2d58f856931258fb5aa6ae10f8aa73785cc
anchal66 <anchalavinash66@gmail.com>
2024-05-19 16:39:20 +0530

    initial commit

---
+++ src/main/java/com/mohit/hospitalmagement/HospitalMagementApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.hospitalmagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalMagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalMagementApplication.class, args);
+    }
+
+}
